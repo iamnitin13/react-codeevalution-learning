634f1e4bf5ed23073aa2ca85b4b53f2541215e37
Nitin Srivastava <nitins1bro@gmail.com>
2023-03-27 09:21:54 +0530

    feat: code evalution basic react learning

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return React.createElement("div", { className: "App" }, <h1>Hello world</h1>);
+}
+
+export default App;
