b792cc147c7accae495c41936e68389126a8e2f2
Nitin Srivastava <nitins1bro@gmail.com>
2023-04-14 14:04:24 +0530

    doc: update the middlware default importance

--- src/app/store.js
+++ src/app/store.js
@@ -15,6 +15,12 @@ const store = configureStore({
   },
   // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
   // getDefaultMiddleware by default add some middleware to our store ;so concat with other middleware list
+
+  /**  if we want to add some middleware it must be concated to getDefaultMiddleware,
+   * like createAsyncThunk uses thunk middlewar internally which is by default provided to it;but when adding any custom middleare like saga,
+   * it must be concated with default ; else we will get error for
+   * i got error for action must be plan object
+   */
 });
 
 export default store;
